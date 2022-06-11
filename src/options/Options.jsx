319f144267c50096796bb879ac908499df1df605
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-11 11:27:07 +0700

    add icons

--- src/options/Options.jsx
+++ src/options/Options.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
+import { Helmet } from 'react-helmet-async';
 import Sidebar from './layout/Sidebar/Sidebar';
 import Main from './layout/Main/Main';
 import { OptionsContainer } from './Options.style';
 import SidebarProvider from '../context/Sidebar.context';
 import { ThemeContext } from '../theme/ThemeProvider';
 import useWebStorage from '../hooks/useWebStorage';
+
+const iconsDir = chrome?.runtime?.id ? `chrome-extension://${chrome?.runtime?.id}/icons` : null;
 
 const Options = () => {
   // If in standalone website use localStorage
@@ -13,12 +16,38 @@ const Options = () => {
   const { theme, mode } = useContext(ThemeContext);
 
   return (
-    <SidebarProvider>
-      <OptionsContainer className={`${theme} ${mode}`}>
-        <Sidebar />
-        <Main />
-      </OptionsContainer>
-    </SidebarProvider>
+    <>
+      {
+        iconsDir && (
+          <Helmet>
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href={`${iconsDir}/apple-touch-icon.png`}
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href={`${iconsDir}/favicon-32x32.png`}
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href={`${iconsDir}/favicon-16x16.png`}
+            />
+          </Helmet>
+        )
+      }
+
+      <SidebarProvider>
+        <OptionsContainer className={`${theme} ${mode}`}>
+          <Sidebar />
+          <Main />
+        </OptionsContainer>
+      </SidebarProvider>
+    </>
   );
 };
 
--- src/options/main.jsx
+++ src/options/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import GlobalStyles from '../styles/GlobalStyles';
 import Options from './Options';
 import ListProvider from '../context/List.context';
@@ -8,17 +9,19 @@ import ThemeProvider from '../theme/ThemeProvider';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ListProvider>
-        <Router
-          baseName="/dist/options"
-          hashType="noslash"
-        >
-          <GlobalStyles />
-          <Options />
-        </Router>
-      </ListProvider>
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider>
+        <ListProvider>
+          <Router
+            baseName="/dist/options"
+            hashType="noslash"
+          >
+            <GlobalStyles />
+            <Options />
+          </Router>
+        </ListProvider>
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById('root-options'),
 );
