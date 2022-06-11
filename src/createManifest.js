e00ecfc5d7a56718824a6b6a9e7165976b457acd
dominggo1999 <arnolddominggo19@gmail.com>
2022-06-11 10:19:02 +0700

    remove mozilla manifest creation

--- src/createManifest.js
+++ src/createManifest.js
@@ -3,20 +3,14 @@ const { resolve } = require('path');
 
 const vendor = process.env.VENDOR;
 const chromeManifest = require('./manifest/chrome-manifest.json');
-const mozillaManifest = require('./manifest/mozilla-manifest.json');
 
 const r = (path) => resolve(__dirname, path);
 
 let manifest;
 let targetDir;
-if(vendor === 'chromium') {
+if (vendor === 'chromium') {
   manifest = chromeManifest;
   targetDir = r('../extension/chromium/manifest.json');
-}
-
-if(vendor === 'mozilla') {
-  manifest = mozillaManifest;
-  targetDir = r('../extension/mozilla/manifest.json');
 }
 
 const writeManifest = async () => {
